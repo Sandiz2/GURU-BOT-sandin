f5cc6e6e5317766747c926d6d2e7b33e31e11230
Sandiz2 <152058290+Sandiz2@users.noreply.github.com>
2023-11-29 15:01:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94704352872', 'SANDIZ', true],
+  ['94704352872', 'Sandin Sathsen', true], 
   [''] 
 ] //Number of owners
 
@@ -50,13 +50,13 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'sᴀɴᴅɪᴢ-ᴏғғɪᴄɪᴀʟ
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'sᴀɴᴅɪᴢ ᴏғᴄ' 
+global.author = '@sᴀɴᴅɪᴢ 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.dygp = 'https://chat.whatsapp.com/BOYKjfV97d73zQTBIACjb3'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
